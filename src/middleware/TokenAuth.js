e0b0a4c1ea337d4a99fbb984c2bee03260b3f4c8
Yug Khokhar <83321546+kyr1234@users.noreply.github.com>
2023-03-28 18:22:49 +0530

    TokenAuth

--- src/middleware/TokenAuth.js
+++ src/middleware/TokenAuth.js
@@ -3,17 +3,8 @@ import jwt from 'jsonwebtoken'
 
 const tokenVerifier = (req, res, next) => {
   try {
-    const Bearer = req.header('Authorization')
-    const token = Bearer.replace('Bearer ', '')
-    if (jwt.verify(token, process.env.JWT_SECRET_KEY)) {
-      next()
-    } else {
-      return apiResponse.unauthorizedResponse(res, 'Access Denied')
-    }
-    // return apiResponse.unauthorizedResponse(res,error);
   } catch (error) {
-    // Access Denied
-    return apiResponse.unauthorizedResponse(res, error)
+
   }
 }
 export default tokenVerifier
