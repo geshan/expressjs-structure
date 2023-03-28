e9c256d070182237fdeda188230d3c946d5690f6
Yug Khokhar <83321546+kyr1234@users.noreply.github.com>
2023-03-28 18:21:45 +0530

    RBAC PANEL

--- src/middleware/Rbac.js
+++ src/middleware/Rbac.js
@@ -1,15 +1,1 @@
-import {  forbiddenResponse } from '../helper/apiResponse.js';
 
-
-export const roleAccess = (allowed = ['*']) => {
-    return (req, res, next) => {
-        
-        const user = req.user_info;
-
-        if (allowed[0] === "*" || (allowed.indexOf(parseInt(user.role)) !== -1)) {
-            next()
-        } else {
-            return forbiddenResponse(res)
-        }
-    }
-}
